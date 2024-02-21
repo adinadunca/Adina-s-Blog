2a51d6f3c1ca45a7dd6e835ac02661ac648b09ba
adinadunca <160733390+adinadunca@users.noreply.github.com>
2024-02-21 13:25:51 +0200

    Add files via upload

---
+++ Blog/Script.js
@@ -0,0 +1,11 @@
+function openArticles() {
+    window.location.href ='Index.html';
+    }
+    
+    function openContact () {
+    window.location.href = 'Contact.html';
+    }
+    
+    function openReviews() {
+    window.location.href = 'Reviews.html';
+}
